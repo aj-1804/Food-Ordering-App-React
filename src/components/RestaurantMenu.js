748955c4d8edbc20b1ea558aa73e21ff76592569
Amit Jha <amitkj.1804@gmail.com>
2024-02-25 17:24:07 +0530

    Completed Lecture 9

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,26 +1,27 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-    const [resInfo, setResInfo] = useState(null);
+    // const [resInfo, setResInfo] = useState(null);
 
     const { resId } = useParams();
 
-    useEffect(() => {
-        fetchMenu();
-    }, []);
+    const resInfo = useRestaurantMenu(resId);
 
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_API + resId);
+    // useEffect(() => {
+    //     fetchMenu();
+    // }, []);
 
-        const json = await data.json();
+    // const fetchMenu = async () => {
+    //     const data = await fetch(MENU_API + resId);
 
-        console.log(json);
+    //     const json = await data.json();
 
-        setResInfo(json.data);
-    };
+    //     console.log(json);
+
+    //     setResInfo(json.data);
+    // };
 
     if (resInfo === null) return <Shimmer />;
 
