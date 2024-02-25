5f6c384792a90db18395c1ee0581f5d2688fdb0c
Amit Jha <amitkj.1804@gmail.com>
2024-02-25 13:12:03 +0530

    Completed Lecture 8

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,33 @@
-const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <h2>This is Namaste React Web Series</h2>
-        </div>
-    );
-};
+import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+import React from "react";
+
+class About extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // console.log("Parent Constructor");
+    }
+
+    componentDidMount() {
+        // console.log("Parent Component Did Mount");
+    }
+
+    render() {
+        // console.log("Parent Render");
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is Namaste React Web Series</h2>
+                {/* <User name={"Amit (Function)"} location={"New Delhi (Function)"} /> */}
+                <UserClass
+                    name={"Amit (Class)"}
+                    location={"New Delhi (Class)"}
+                />
+            </div>
+        );
+    }
+}
 
 export default About;
